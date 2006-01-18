1fd7863ad9081346bf9c69054b22efa219c334d6
nobunobuta <nobunobuta@nobunobuta.com>
2006-01-18 12:33:58 +0000

    一部微調整

--- modules/mygmap/class/mygmap_classes.php
+++ modules/mygmap/class/mygmap_classes.php
@@ -3,4 +3,5 @@ require_once (XOOPS_ROOT_PATH.'/modules/mygmap/class/xoopstableobject.php');
 require_once (XOOPS_ROOT_PATH.'/modules/mygmap/class/mygmap_marker.php');
 require_once (XOOPS_ROOT_PATH.'/modules/mygmap/class/mygmap_category.php');
 require_once (XOOPS_ROOT_PATH.'/modules/mygmap/class/mygmap_area.php');
+require_once (XOOPS_ROOT_PATH.'/modules/mygmap/include/functions.php');
 ?>
