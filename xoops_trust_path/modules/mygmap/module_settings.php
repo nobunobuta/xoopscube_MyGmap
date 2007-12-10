2f86686ea523e64b56679e8ab0739f6631125304
nobunobuta <nobunobuta@nobunobuta.com>
2007-12-10 01:30:17 +0000

    Thumbs.dbをcvs:ignoreに追加

--- xoops_trust_path/modules/mygmap/module_settings.php
+++ xoops_trust_path/modules/mygmap/module_settings.php
@@ -13,16 +13,10 @@ if (class_exists('NBFrame')) {
                                                       'admin.MyGmapAreaAdmin',
                                                       'admin.MyGmapMarkerAdmin',
                                                       'admin.MyGmapCategoryAdmin',
-                                                      'NBFrame.GetImage',
-                                                      'NBFrame.GetModuleIcon',
-                                                      'NBFrame.admin.BlocksAdmin',
-                                                      'NBFrame.admin.AltSys',
                                                   ));
 
     $environment->setAttribute('NoCommonAction', array('MyGmapHttpReq',
                                                        'MyGmapLoadJscipt',
-                                                       'NBFrame.GetImage',
-                                                       'NBFrame.GetModuleIcon',
                                                        ));
 
     $environment->setAttribute('BlockHandler', array('MyGmapMiniMapBlock'));
